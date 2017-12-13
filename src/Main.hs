50f86f2fbb85b19285e596f43acd3c80ec30989c
ice1000 <ice1000kotlin@foxmail.com>
2017-12-13 12:15:11 +0800

    fix compilation error

--- src/Main.hs
+++ src/Main.hs
@@ -10,9 +10,5 @@ main = let file = "in.mbg" in do
   allCodes <- readFile file
   putStrLn "File contents:"
   putStrLn allCodes
-  print $ p <!-- allCodes
-  where p = do
-          f <- crazyDmslP
-          newLines0P
-          return f
+  print $ crazyDmslP <!-- allCodes
 --
